25b6c9098ea65aebe9439bd60736cd902d22f0c7
Quang Nguyen <phamthaiquangnguyen0612@gmail.com>
2023-09-28 10:33:39 +0700

    GET/POST method for web api

--- web_ui/main.js
+++ web_ui/main.js
@@ -16,11 +16,35 @@ async function run() {
   }
 }
 
-async function searching() {
+async function searching() 
+{
+    /*
     var db = await connectToDatabase();
     //document.getElementById('input')
     const product_name = document.getElementById('input').innerHTML;
     await createDocuments(db, 'fromNode', {name: product_name});
+    */
+    var base_url = document.getElementById("input").textContent;
+    var xhr = new XMLHttpRequest();
+    // Create an object with the data you want to send
+    var dataToSend = {
+        from: "web browser"
+    };
+    var url = base_url + "?date=27/09/2023";
+    var jsonData = JSON.stringify(dataToSend);
+    xhr.open("GET", url);
+    xhr.setRequestHeader("Content-Type", "application/json"); // Specify JSON content type
+    xhr.responseType = 'json';
+    xhr.send(jsonData);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        const response = JSON.stringify(xhr.response);
+        document.getElementById("content").innerHTML = response;
+      } else {
+        document.getElementById("content").innerHTML = "Error loading URL";
+      }
+    };
+  });
 }
 async function main() {
     var db = await connectToDatabase();  
