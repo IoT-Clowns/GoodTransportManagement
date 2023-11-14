cb3af6c0c3a9150fb1608bedad2c84714b08d266
j3r3my0612 <phamthaiquangnguyen0612@gmail.com>
2023-11-14 10:46:24 +0700

    search bar handling

--- web_ui/public/js/control.js
+++ web_ui/public/js/control.js
@@ -41,3 +41,38 @@ orderChecker.addEventListener("change", function () {
 })
 
 
+const search_text = document.getElementById("product_searching");
+search_text.addEventListener("change", function () {
+    var text = search_text.value;
+    console.log(text);
+    var base_url = "https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-xtvas/endpoint/web_browser/product_searching";
+
+    if(productChecker.checked == 1) // product searching
+    {
+        var url = base_url + "?pattern=" + text;
+    }
+    var xhr = new XMLHttpRequest();
+    var dataToSend = {
+        from: "web browser"
+    };
+    var jsonData = JSON.stringify(dataToSend);
+    xhr.open("GET", url);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.responseType = 'json';
+    xhr.send(jsonData);
+    xhr.onload = function() 
+    {
+        if (xhr.status === 200) 
+        {
+            var data = xhr.response;
+            alert(data);
+            console.log(data);
+            const element = document.getElementById('search-result');
+            element.style.display = 'block'; // Hide the element
+        }
+    };
+})
+
+
+
+
